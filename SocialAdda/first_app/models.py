07742ce1a4bcb343165ce86f62826e85a0251038
Dheeraj Devaraj <dheeraj020602@gmail.com>
2022-02-13 22:43:50 +0400

    Added visible field, set to hide new confessions.

--- SocialAdda/first_app/models.py
+++ SocialAdda/first_app/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Conf(models.Model):
+    votes = models.IntegerField(default=0)
     text = models.TextField()
+    visible = models.BooleanField(default=0)
 
     def __str__(self):
         return self.text
+
