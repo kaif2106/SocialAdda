87a763f862ba8cae6caadcd924a70260437afb6b
Mohammed Kaif <mohammedkaif2106@gmail.com>
2022-02-15 23:53:00 +0400

    front list

--- SocialAdda/first_app/forms.py
+++ SocialAdda/first_app/forms.py
@@ -5,8 +5,26 @@ from first_app.models import Conf
 class formName(forms.ModelForm):
     class Meta:
         model = Conf
-        fields = ['text']
-        text = forms.CharField(max_length=100)
+        fields = ['heading', 'text']
+        
+        widgets = {
+            'heading': forms.Textarea(attrs={
+                'class':'',
+                'style':'max-width: 900px; color: white; background-color: #1a1a1b; resize: none;',
+                'placeholder':'Heading',
+                'rows':'1',
+                'cols':'90'
+            }),
+            'text': forms.Textarea(attrs={
+                'class':'',
+                'style':'max-width: 900px; color: white; background-color: #1a1a1b;',
+                'placeholder':'Text',
+                'cols': '90'
+            })
+        }
+        # heading = forms.CharField(max_length=200)
+        # text = forms.CharField(max_length=100)
+        
 
 class voteForm(forms.Form):
     upVote = forms.BooleanField()
--- SocialAdda/first_app/models.py
+++ SocialAdda/first_app/models.py
@@ -4,9 +4,11 @@ from django.db import models
 # Create your models here.
 class Conf(models.Model):
     votes = models.IntegerField(default=0)
-    text = models.TextField()
+    heading = models.CharField(max_length=150)
+    text = models.TextField(max_length=500)
     visible = models.BooleanField(default=False)
     visibleTime = models.TimeField(auto_now=True)
+    
 
     class Meta:
         ordering = ['-visibleTime']
